3620548f4a839b592b7e5326f1e05916d1d92241
Bryanna-21 <bryannamanu@gmail.com>
2026-04-24 11:52:25 +0300

    Create Profile.js

---
+++ pages/Profile.js
@@ -0,0 +1,13 @@
+import MainLayout from "../layout/MainLayout";
+
+export default function Profile() {
+  return (
+    <MainLayout>
+      <div className="card">
+        <h2>User Profile</h2>
+        <p>Name: Coming soon</p>
+        <p>University: Coming soon</p>
+      </div>
+    </MainLayout>
+  );
+}
