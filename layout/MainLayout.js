3686f414ebe26b2c7d83359dded8e497ad0236ec
Bryanna-21 <bryannamanu@gmail.com>
2026-04-24 12:33:16 +0300

    Update MainLayout.js

--- layout/MainLayout.js
+++ layout/MainLayout.js
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import Rightbar from "../components/Rightbar";
 
 export default function MainLayout({ children }) {
   return (
     <>
       <Navbar />
-      <div className="main">
+      <div style={{ display: "flex" }}>
         <Sidebar />
         <div className="content">{children}</div>
+        <Rightbar />
       </div>
     </>
   );
