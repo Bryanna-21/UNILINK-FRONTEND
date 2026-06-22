8752c5e874234cb537050c21326ce4fa4214e3b4
Bryanna-21 <bryannamanu@gmail.com>
2026-06-22 12:25:43 +0300

    Create Loader.js

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+export default function Loader() {
+  return (
+    <div
+      style={{
+        padding: "30px",
+        textAlign: "center"
+      }}
+    >
+      Loading...
+    </div>
+  );
+}
