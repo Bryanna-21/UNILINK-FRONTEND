cfafff75305f16075ec35736c1b64a80187ea771
Bryanna-21 <bryannamanu@gmail.com>
2026-06-22 12:44:41 +0300

    Create NotificationContext.js

---
+++ src/context/NotificationContext.js
@@ -0,0 +1,40 @@
+import {
+  createContext,
+  useContext,
+  useState
+} from "react";
+
+const NotificationContext =
+  createContext();
+
+export const NotificationProvider = ({
+  children
+}) => {
+  const [notifications,
+    setNotifications] =
+    useState([]);
+
+  const addNotification =
+    (notification) => {
+      setNotifications(prev => [
+        ...prev,
+        notification
+      ]);
+    };
+
+  return (
+    <NotificationContext.Provider
+      value={{
+        notifications,
+        addNotification
+      }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export const useNotifications =
+  () => useContext(
+    NotificationContext
+  );
