143fcee5fd8c22542d49dbfc7cfdd85f58529563
Bryanna-21 <bryannamanu@gmail.com>
2026-06-22 14:53:50 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
-    "http://localhost:5000/api"
+    "http://localhost:5000/api",
 });
 
 API.interceptors.request.use(
@@ -17,6 +17,24 @@ API.interceptors.request.use(
     }
 
     return config;
+  }
+);
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+
+    if (
+      error.response &&
+      error.response.status === 401
+    ) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
   }
 );
 
