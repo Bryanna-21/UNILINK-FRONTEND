e110bd38e8dd1d337f401868818840b32db3e3be
Bryanna-21 <bryannamanu@gmail.com>
2026-06-22 12:37:53 +0300

    Create LecturerDashboard.js

---
+++ src/pages/LecturerDashboard.js
@@ -0,0 +1,17 @@
+import MainLayout from "../layout/MainLayout";
+
+export default function LecturerDashboard() {
+  return (
+    <MainLayout>
+      <h2>Lecturer Dashboard</h2>
+
+      <div className="card">
+        <h3>Course Communities</h3>
+      </div>
+
+      <div className="card">
+        <h3>Announcements</h3>
+      </div>
+    </MainLayout>
+  );
+}
