dbda800f0c1b5fc49a66555bdbcb2fe2c3d0299d
Bryanna-21 <bryannamanu@gmail.com>
2026-06-22 14:55:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+import { AuthProvider }
+from "./context/AuthContext";
+
+import { NotificationProvider }
+from "./context/NotificationContext";
+
+const root =
+  ReactDOM.createRoot(
+    document.getElementById("root")
+  );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );
