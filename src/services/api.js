97ad1c40b445ff1e9185c7d6eb12c7b516e2219b
Bryanna-21 <bryannamanu@gmail.com>
2026-06-23 17:22:33 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,52 +1,29 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL:
-process.env
-.REACT_APP_BACKEND_URL ||
-"http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use(
-(config) => {
-const token =
-localStorage.getItem(
-"token"
-);
-
-```
-if (token) {
-  config.headers.Authorization =
-    `Bearer ${token}`;
-}
-
-return config;
-```
-
-}
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  }
 );
 
 API.interceptors.response.use(
-(response) =>
-response,
-(error) => {
-if (
-error.response
-?.status === 401
-) {
-localStorage.clear();
-
-```
-  window.location.href =
-    "/";
-}
-
-return Promise.reject(
-  error
-);
-```
-
-}
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default API;
