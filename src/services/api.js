0f38b08207d023f01d96c806c618cf99cbd063dc
Bryanna-21 <bryannamanu@gmail.com>
2026-06-23 18:21:26 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.REACT_APP_BACKEND_URL ||
-    "http://localhost:5000/api",
+    "http:unilink-backend.railway.internal",
 });
 
 API.interceptors.request.use(
