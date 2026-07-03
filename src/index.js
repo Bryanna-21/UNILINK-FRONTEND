b9a8f94a13008c3593105ec0a1c571894dd441a1
Bryanna-21 <bryannamanu@gmail.com>
2026-07-03 10:24:24 +0300

    Replace NotificationProvider with Toaster component
    Removed NotificationProvider and added Toaster for notifications.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App";
-
 import { AuthProvider } from "./context/AuthContext";
-import { NotificationProvider } from "./context/NotificationContext";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <NotificationProvider>
-          <App />
-        </NotificationProvider>
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            duration: 4000,
+          }}
+        />
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
