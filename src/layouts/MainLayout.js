acbaf1ec63356a26f05a19c4d94e8d226f479775
Bryanna-21 <bryannamanu@gmail.com>
2026-07-03 15:00:52 +0300

    Integrate EmergencyMenu into MainLayout
    Added EmergencyMenu component to MainLayout.

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,33 +1,20 @@
 import { Outlet } from "react-router-dom";
-
 import Sidebar from "../components/layout/Sidebar";
-
 import Topbar from "../components/layout/Topbar";
-
+import EmergencyMenu from "../components/Emergency";
 import "../styles/layout/layout.css";
 
-export default function MainLayout(){
-
-return(
-
-<div className="layout">
-
-<Sidebar/>
-
-<div className="main">
-
-<Topbar/>
-
-<div className="content">
-
-<Outlet/>
-
-</div>
-
-</div>
-
-</div>
-
-);
-
+export default function MainLayout() {
+  return (
+    <div className="layout">
+      <Sidebar />
+      <div className="main">
+        <Topbar />
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+      <EmergencyMenu />
+    </div>
+  );
 }
