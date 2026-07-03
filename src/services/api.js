d9bad09539ad234f2cfe7ec2b68d4ac5c79dfd86
Bryanna-21 <bryannamanu@gmail.com>
2026-07-03 09:14:09 +0300

    Change API base URL to localhost:8080

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.REACT_APP_BACKEND_URL ||
-    "http://localhost:5000/api",
+    "http://localhost:8080/api",
 });
 
 API.interceptors.request.use(
