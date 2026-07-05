7878402745afb993322a53b21c0b8859c6fb2f90
Bryanna-21 <bryannamanu@gmail.com>
2026-07-05 21:56:08 +0300

    Create Search.js

---
+++ src/pages/Search.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+export default function Search() {
+  const [query, setQuery] = useState("");
+
+  const sampleResults = [
+    "Computer Science Community",
+    "Software Engineering Event",
+    "John Doe"
+  ];
+
+  const filtered = sampleResults.filter(item =>
+    item.toLowerCase().includes(query.toLowerCase())
+  );
+
+  return (
+    <>
+      <h2>Search</h2>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) =>
+          setQuery(e.target.value)
+        }
+      />
+
+      {filtered.map((item, index) => (
+        <div className="card" key={index}>
+          {item}
+        </div>
+      ))}
+    </>
+  );
+}
