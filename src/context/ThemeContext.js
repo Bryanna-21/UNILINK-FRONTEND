a1db7cdd1ec29fb4c982d433ce3d24d1e18193cf
Bryanna-21 <bryannamanu@gmail.com>
2026-07-07 10:20:28 +0300

    Create ThemeContext.js

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,40 @@
+import React, {
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const savedTheme =
+    localStorage.getItem("theme") || "light";
+
+  const [theme, setTheme] = useState(savedTheme);
+
+  useEffect(() => {
+    document.body.setAttribute(
+      "data-theme",
+      theme
+    );
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) =>
+      prev === "light" ? "dark" : "light"
+    );
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
