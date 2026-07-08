91b0950e671ee42c595d632ebb19d059277a489b
Bryanna-21 <bryannamanu@gmail.com>
2026-07-08 15:29:16 +0300

    Create useExam.js

---
+++ src/hooks/useExam.js
@@ -0,0 +1,7 @@
+// =======================================================
+// UniLink Exam Hook
+// Centralized Exam State Management
+// =======================================================
+
+import { useState, useCallback } from "react";
+import *
