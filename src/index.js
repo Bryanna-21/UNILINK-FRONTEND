bd4654a2535f7c4e43c1d576ffa5a2cabcb68cb6
Bryanna-21 <bryannamanu@gmail.com>
2026-07-09 13:17:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,72 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
 
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-
-import "./index.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
-import "./index.css";
-
 import Toast from "./components/ui/Toast";
 
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import { NotificationProvider } from "./context/NotificationContext";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
+import "./index.css";
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-
-      <ThemeProvider>
-
-        <AuthProvider>
-
-          <NotificationProvider>
-
-            <App />
-
-            <Toast />
-
-          </NotificationProvider>
-
-        </AuthProvider>
-
-      </ThemeProvider>
-
-    </BrowserRouter>
-  </React.StrictMode>
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <Toaster
-          position="top-right"
-          reverseOrder={false}
-          toastOptions={{
-            duration: 4000,
-            style: {
-              borderRadius: "10px",
-              background: "#1f2937",
-              color: "#fff",
-            },
-          }}
-        />
-        <App />
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <NotificationProvider>
+            <App />
+            <Toast />
+          </NotificationProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
