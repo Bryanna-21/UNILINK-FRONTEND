e7dea39fbf98c451940d448d6cbaa7d46bda72a6
Bryanna-21 <bryannamanu@gmail.com>
2026-07-12 15:36:20 +0300

    Update Results.js

--- src/pages/Student/Results.js
+++ src/pages/Student/Results.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
+import { toast } from "react-hot-toast";
 
 import ExamResult from "../../components/exams/ExamResult";
 
