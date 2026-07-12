85f0deaf337b0bb1818571db4df311c80db86a6a
Bryanna-21 <bryannamanu@gmail.com>
2026-07-12 15:37:04 +0300

    Update TakeExam.js

--- src/pages/Student/TakeExam.js
+++ src/pages/Student/TakeExam.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { toast } from "react-toastify";
+import { toast } from "react-hot-toast";
 
 import {
   getExamById,
