ff5c658987f27a21e80bc1c307c50930a5cea5f3
Bryanna-21 <bryannamanu@gmail.com>
2026-07-30 10:14:24 +0300

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -86,6 +86,7 @@ export function AuthProvider({ children }) {
 
       return {
         success: true,
+        user: data.user,
       };
     } catch (error) {
       return {
@@ -121,6 +122,7 @@ export function AuthProvider({ children }) {
 
       return {
         success: true,
+        user: data.user,
       };
     } catch (error) {
       return {
